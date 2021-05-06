f1d593da527bc21e8236494b7f92e76943d43b36
hendrasoewarno <hendrasoewarno@gmail.com>
2021-05-06 20:22:31 +0700

    Update dnssvr.py

--- dnssvr.py
+++ dnssvr.py
@@ -3,7 +3,7 @@ Hendra Soewarno (0119067305)
 Honeypot DNS ini tidak disertai pembatasan jumlah thread, sehingga perlu dilakukan
 pembatasan pada level firewall.
 /sbin/iptables  -A INPUT -p udp --syn --dport 5353 -m connlimit --connlimit-above 50 -j REJECT
-DNS mensimulasikan server ftp untuk sebagai honeypot yang menarik penyerang
+DNS mensimulasikan server dns untuk sebagai honeypot yang menarik penyerang
 untuk melakukan bruteforce password. Honeypot akan merekam semua userid dan password
 yang dicoba penyerang, sehingga menjadi early warning bagi administrator terkait dengan
 userid/password yang compromis.
