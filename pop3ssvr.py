011007585df3d19e6c4ab50f8731401ecd3a360d
hendrasoewarno <hendrasoewarno@gmail.com>
2021-05-13 09:18:28 +0700

    Update pop3ssvr.py

--- pop3ssvr.py
+++ pop3ssvr.py
@@ -3,7 +3,7 @@ Hendra Soewarno (0119067305)
 Honeypot pop3s ini tidak disertai pembatasan jumlah thread, sehingga perlu dilakukan
 pembatasan pada level firewall.
 /sbin/iptables  -A INPUT -p tcp --syn --dport 9995 -m connlimit --connlimit-above 50 -j REJECT
-POPSvr mensimulasikan server pop untuk sebagai honeypot yang menarik penyerang
+POPSSvr mensimulasikan server pop3s untuk sebagai honeypot yang menarik penyerang
 untuk melakukan bruteforce password. Honeypot akan merekam semua userid dan password
 yang dicoba penyerang, sehingga menjadi early warning bagi administrator terkait dengan
 userid/password yang compromis.
@@ -18,6 +18,13 @@ import os
 import logging
 from logging.handlers import TimedRotatingFileHandler
 from _thread import *
+import base64
+
+def plainText(auth):
+    auth_bytes = auth.encode('ascii')
+    base64_bytes = base64.b64encode(auth_bytes)
+    base64_message = base64_bytes.decode('ascii')
+    return base64_message
 
 def readRequest(conn):
     text = b""
@@ -52,34 +59,74 @@ def threaded_client(conn, address, count, logger):
     conn.sendall(strwelcome)
     userid = ""
     password = ""
-    auth = 0
+    step = 0
+    
     try:
         while True:       
             request=readRequest(conn)
             logger.info(request) 
             print(request)
             response='-ERR\r\n'
-            if request.startswith(b"QUIT"):
+            if request.upper().startswith(b"QUIT"):
                 response='+OK signing off\r\n';
-            elif request.startswith(b"USER"):
+            elif request.upper().startswith(b"CAPA"):
+                response='+OK\r\nSTLS\r\nUSER\r\nSASL PLAIN LOGIN\r\n';                
+            #start user/pass
+            elif request.upper().startswith(b"USER"):
                 cmd = request.decode("utf-8")
                 userid = cmd[5:-2]
+                step=1
                 response='+OK Password required\r\n';
-            elif request.startswith(b"PASS"):
+            elif request.upper().startswith(b"PASS") and step==2:
                 cmd = request.decode("utf-8")
                 password = cmd[5:-2]
-                #if userid=="root" and password=="password":
-                if password=="password":
-                    auth=1
-                    response='+OK logged in.\r\n'
-            elif auth==1:
-                if request.startswith(b"STAT"):
+                if userid=="root" and password=="password":
+                    step=6
+                    response='+OK Logged in\r\n'
+                else:
+                    step=0
+                    response='-ERR Authentication failed\r\n'
+            #end user/pass
+            #start auth plain
+            elif request.upper().startswith(b"AUTH PLAIN"):
+                response = "+\r\n"
+                step=3
+            elif step==3:    
+                cmd = request.decode("utf-8")
+                auth = cmd[0:-2]
+                if auth==plainText("\0root\0password"):
+                    step=6
+                    response = "+OK Logged in\r\n"
+                else:
+                    response = "-ERR Authentication failed\r\n"
+            #end auth plain            
+            #start auth login
+            elif request.upper().startswith(b"AUTH LOGIN"):
+                response = "+ VXNlcm5hbWU6\r\n"
+                step=4
+            elif step==4:
+                userid = request.decode("utf-8")[0:-2]
+                response = "+ UGFzc3dvcmQ6\r\n"
+                step=5
+            elif step==5:
+                password = request.decode("utf-8")[0:-2]
+                if userid==plainText("root") and password==plainText("password"):
+                    step=6
+                    response = "+OK Logged in\r\n"
+                else:
+                    response = "-ERR Authentication failed\r\n"              
+                step = 0
+            #end auth login
+            elif step==6:
+                if request.upper().startswith(b"STAT"):
                     response='+OK 1 100\r\n'
-                elif request.startswith(b"LIST") or request.startswith(b"TOP"):
+                elif request.upper().startswith(b"LIST") or request.upper().startswith(b"TOP"):
                     response='+OK 1 messages\r\n1 100\r\n.\r\n'
-                elif request.startswith(b"RETR"):
+                elif request.upper().startswith(b"RETR"):
                     response='+OK messages follows\r\nDate: Fri, 26 Mar 2021 10:58:23 +0700\r\nFrom: admin@localhost\r\nTo: root@localhost\r\nContent-Type: text/plain; charset="utf-8"\r\nSubject: This is Testmail\r\n\r\nHello, mail from you Administrator.\r\nPlease ignore it.\r\n.\r\n'
-                elif request.startswith(b"DELE"):
+                elif request.upper().startswith(b"RSET"):
+                    response='+OK\r\n'		                    
+                elif request.upper().startswith(b"DELE"):
                     response='+OK Message deleted\r\n'				
 
             print(response)					
@@ -141,4 +188,3 @@ while True:
         print(str(e))
 
 ServerSocket.close()
-
